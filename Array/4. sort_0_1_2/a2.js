7c9937b2427c742d7251a7bbcda23653fcd430dd
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2022-11-01 21:16:05 +0530

    sort 0,1,2 apprach 2

--- Array/4. sort_0_1_2/a2.js
+++ Array/4. sort_0_1_2/a2.js
@@ -1,2 +1,31 @@
 // Sort an array of 0s, 1s and 2s
 
+let arr = [0,2,1,2,0];
+
+let zero = 0;
+let one = 0;
+let two = 0;
+
+for(let i=0 ; i<arr.length ; i++){
+    if(arr[i]===0)
+        zero++;
+    else if(arr[i]===1)
+        one++;
+    else
+        two++;
+}
+
+let output = [];
+
+for(let i=0 ; i<zero ; i++){
+    output.push(0);
+}
+
+for(let i=0 ; i<one ; i++){
+    output.push(1);
+}
+
+for(let i=0 ; i<two ; i++){
+    output.push(2);
+}
+console.log(output);
